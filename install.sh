7d9478ff32623d9df7c62546365403c7d0a4010c
mts299 <mts299888@gmail.com>
2018-01-26 13:23:39 -0600

    made some changes to add the next and prev fitacf file, however we decide to create a python version of this script so I am moving to new branch

--- install.sh
+++ install.sh
@@ -4,24 +4,18 @@
 #
 #This script is used to setup the bash environmnent variables for the use of mappot_fitacf 
 #
-export PATH=`pwd`:$PATH
+mkdir ~/bin 
+cp ./src/mappot_fitacf ./bin/mappot_fitacf
+
+export PATH=`pwd`/bin/:$PATH >>> .bashrc 
 #Assumes this is the default key file
-export KEYFILE="rainbow.key"
+export KEYFILE="rainbow.key" >>> .bashrc 
 
 if [ -f "./north_extensions" ] && [ -f "./south_extensions" ]
 then 
-    export MAPPOTEXTENSIONSPATH=`pwd`
+    export MAPPOTEXTENSIONSPATH=`pwd`/file_extensions >>> .bashrc  
 else 
     echo "Please set the environment variable MAPPOTEXTENSIONSPATH to where the extensions files are located"
     echo "To set the variable use the command: export MAPPOTEXTENSIONSPATH=path/to/mappot/extensions/file"
-fi
-
-if [ -f "./rainbow.key" ]
-then
-    export KEYFILEPATH=`pwd`
-else 
-    echo "Please set the environment variable KEYFILEPATH to where the key file is located"
-    echo "To set the variable use the command: export KEYFILEPATH=path/to/key/file"
-    echo "If you wish to not use a keyfile then the default grey scale will be used."
 fi
 
