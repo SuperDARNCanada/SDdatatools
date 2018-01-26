acc62f87f86b40f9f6860ae993828dc7baae404b
mts299 <mts299888@gmail.com>
2018-01-26 16:33:31 -0600

    started python branch to convert the mappot_fitacf bash script to python for readability purposes

---
+++ src/fitacf2convectionMap.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# Copyright 2017 SuperDARN Canada
+#
+# Marina Schmidt
+#
+# fitacf2convectionMap.py
+# 2018-01-26
+
+import os
+import date
+import pycurl
+import sys
+
+
+
