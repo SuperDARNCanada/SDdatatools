3d7b1466ac367f6eee957e0fb2a19e4be9518801
mts299 <mts299888@gmail.com>
2018-08-23 11:11:59 -0600

    made some changes for importing classes with python3

--- DARNprocessing/__init__.py
+++ DARNprocessing/__init__.py
@@ -1,11 +1,1 @@
-import plotting_scripts
-from plotting_scripts.convectionmaps import ConvectionMaps
-
-import IMF_scripts
-from IMF_scripts.omni import Omni
-
-import utils
-from utils import convectionMapConstants
-from utils import convectionMapExceptions
-from utils import convectionMapWarnings
-from utils import utils
+from .plotting_scripts.convectionmaps import ConvectionMaps
